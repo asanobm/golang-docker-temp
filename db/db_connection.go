d5f0aed05de273b41212be88087c1e2300091a20
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-25 13:32:43 +0900

    remove: dotenv

--- db/db_connection.go
+++ db/db_connection.go
@@ -6,15 +6,10 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
-	"github.com/joho/godotenv"
 )
 
 // Conn function can get a database connection.
 func Conn() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Environment variables load error.")
-	}
 	db, err := gorm.Open(
 		"postgres",
 		"host="+os.Getenv("DATABASE_HOST") +" "+
@@ -24,12 +19,7 @@ func Conn() *gorm.DB {
 		"password="+os.Getenv("DATABASE_PASSWORD") +" "+
 		"sslmode=disable")
 	if err != nil {
-		log.Fatalln(err)
-		log.Println(os.Getenv("DATABASE_HOST"))
-		log.Println(os.Getenv("DATABASE_NAME"))
-		log.Println(os.Getenv("DATABASE_USER"))
-		log.Println(os.Getenv("DATABASE_PASSWORD"))
-		log.Println(os.Getenv("DATABASE_PORT"))
+		log.Panicln(err)
 	}
 	log.Println("Connected Database")
 	return db
